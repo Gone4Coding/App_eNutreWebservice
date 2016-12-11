efc2dcfe8b287258ecaf6068742764bb3676d2f8
Gone4Coding <gongas.gordalina14@gmail.com>
2016-12-11 01:10:44 +0000

    asd

--- WebserviceAppNutre/WebserviceAppNutre/ServiceAppNutre.svc.cs
+++ WebserviceAppNutre/WebserviceAppNutre/ServiceAppNutre.svc.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Web.Hosting;
 using System.Xml;
 
 namespace WebserviceAppNutre
@@ -13,6 +15,112 @@ namespace WebserviceAppNutre
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class ServiceAppNutre : IServiceAppNutre
     {
+        private Dictionary<string, User> users;
+        private Dictionary<string, Token> tokens;
+
+        private static string FILEPATH;
+
+        public ServiceAppNutre()
+        {
+            users = new Dictionary<string, User>();
+            tokens = new Dictionary<string, Token>();
+            fillUsers();
+
+            FILEPATH = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "App_Data", "users.xml");
+        }
+
+        private class Token
+        {
+            private string value;
+            private long timeout;
+            private User user;
+
+            public Token(User user)
+                : this(user, 240000) // token válido por 4 minutos
+            {
+            }
+
+            public Token(User user, long timeout)
+            {
+                this.value = Guid.NewGuid().ToString();
+                this.timeout = Environment.TickCount + timeout;
+                this.user = user;
+            }
+
+            public string Value
+            {
+                get { return value; }
+            }
+
+            public long Timeout
+            {
+                get { return timeout; }
+            }
+
+            public User User
+            {
+                get { return user; }
+            }
+
+            public string Username
+            {
+                get { return user.Username; }
+            }
+
+            public void UpdateTimeout()
+            {
+                UpdateTimeout(240000); // token renovado por 4 minutos
+            }
+
+            public void UpdateTimeout(long timeout)
+            {
+                this.timeout = Environment.TickCount + timeout;
+            }
+
+            public Boolean isTimeoutExpired()
+            {
+                return Environment.TickCount > timeout;
+            }
+        }
+
+        private void cleanUpTokens()
+        {
+            foreach (Token tokenObject in tokens.Values)
+            {
+                if (tokenObject.isTimeoutExpired())
+                {
+                    tokens.Remove(tokenObject.Username);
+                }
+            }
+        }
+
+        /*private bool isUserValid(string username, string password)
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+
+            XmlNode passNode = doc.SelectSingleNode("//user[username = '" + username + "']//password");
+
+            return passNode != null && passNode.InnerText.Equals(password);
+        }*/
+
+        private void fillUsers()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNodeList usersList = doc.SelectNodes("//user");
+
+            foreach (XmlNode user in usersList)
+            {
+                string username = user.SelectSingleNode("//username").InnerText;
+                string password = user.SelectSingleNode("//pasword").InnerText;
+                bool isAdmin = Convert.ToBoolean(user.SelectSingleNode("//@isAdmin").InnerText);
+
+                users.Add(username, new User(username, password, isAdmin));
+            }
+        }
+
         public void SignUp(User user, string token)
         {
             throw new NotImplementedException();
@@ -20,12 +128,21 @@ namespace WebserviceAppNutre
 
         public void LogIn(string username, string password)
         {
-            throw new NotImplementedException();
+            cleanUpTokens();
+
+            if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) && password.Equals(users[username].Password))
+            {
+                Token tokenObject = new Token(users[username]);
+                tokens.Add(tokenObject.Value, tokenObject);
+            }
+            throw new ArgumentException("ERROR: invalid username/password combination.");
+
         }
 
         public void LogOut(string token)
         {
-            throw new NotImplementedException();
+            tokens.Remove(token);
+            cleanUpTokens();
         }
 
         public void addActivity(Activity activity, string token)
